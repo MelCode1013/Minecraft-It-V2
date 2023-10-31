5ae5b560a03f1b01a0512990bd52d47d0679da3c
Melinna Bayona <melinna.dev@gmail.com>
2023-10-31 16:32:26 +0000

    created homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import '../global/globalStyle.css';
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+        <h1>Welcome to Minecraft It!</h1>
+        <p>A place for people who love Minecraft to come together and share their builds</p>
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
